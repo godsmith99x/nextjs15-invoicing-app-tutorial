062804a6f4e3fe119f75e095d8539032d45bb2a0
godsmith <j.godsmith@proton.me>
2024-10-29 14:46:04 -0400

    Initialize Next.js project with TypeScript, ESLint, Tailwind CSS, and PostgreSQL setup

---
+++ app/invoices/new/page.tsx
@@ -0,0 +1,53 @@
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
+import { sql } from "drizzle-orm";
+import { db } from "@/db";
+
+export default async function NewInvoices() {
+  const invoices = await db.execute(sql`SELECT current_database()`);
+
+  return (
+    <main className="flex flex-col justify-center gap-6 h-full max-w-5xl mx-auto my-12">
+      <div className="flex justify-between">
+        <h1 className="text-3xl font-bold">Create a New Invoice</h1>
+      </div>
+
+      {JSON.stringify(invoices)}
+
+      <form className="grid gap-4 max-w-xs">
+        <div>
+          <Label htmlFor="name" className="block font-semibold text-sm mb-2">
+            Billing Name
+          </Label>
+          <Input id="name" name="name" type="text" />
+        </div>
+        <div>
+          <Label htmlFor="email" className="block font-semibold text-sm mb-2">
+            Billing Email
+          </Label>
+          <Input id="email" name="email" type="email" />
+        </div>
+        <div>
+          <Label htmlFor="value" className="block font-semibold text-sm mb-2">
+            Value
+          </Label>
+          <Input id="value" name="value" type="text" />
+        </div>
+        <div>
+          <Label
+            htmlFor="description"
+            className="block font-semibold text-sm mb-2"
+          >
+            Description
+          </Label>
+          <Textarea id="description" name="description"></Textarea>
+        </div>
+        <div>
+          <Button className="w-full font-semibold">Submit</Button>
+        </div>
+      </form>
+    </main>
+  );
+}

3199be73880eadbc0e456c12274d9cfd7611071d
Joel Godfrey-Smith <joel.godfrey-smith@us.labn.net>
2024-11-07 13:51:48 -0500

    Fetch and display invoices in the Dashboard component with dynamic data rendering

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -11,10 +11,13 @@ import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { CirclePlus } from "lucide-react";
 import Link from "next/link";
+import { db } from "@/db";
+import { Invoices } from "@/db/schema";
 
-export default function Dashboard() {
+export default async function Dashboard() {
+  const invoices = await db.select().from(Invoices);
   return (
-    <main className="flex flex-col justify-center gap-6 h-full text-center max-w-5xl mx-auto my-12 mx-3">
+    <main className="flex flex-col justify-center gap-6 h-full text-center max-w-5xl mx-3 my-12">
       <div className="flex justify-between">
         <h1 className="text-3xl font-bold">Invoices</h1>
         <p>
@@ -38,23 +41,46 @@ export default function Dashboard() {
           </TableRow>
         </TableHeader>
         <TableBody>
-          <TableRow>
-            <TableCell className="font-medium text-left p-4">
-              <span className="font-semibold">10/31/2024</span>
-            </TableCell>
-            <TableCell className="text-left p-4">
-              <span className="font-semibold">Bob Belcher</span>
-            </TableCell>
-            <TableCell className="text-left p-4">
-              <span>bob@bobsburgers.com</span>
-            </TableCell>
-            <TableCell className="text-center p-4">
-              <Badge className="rounded-full">Paid</Badge>
-            </TableCell>
-            <TableCell className="text-right p-4">
-              <span className="font-semibold">$250.00</span>
-            </TableCell>
-          </TableRow>
+          {invoices.map((invoice) => {
+            return (
+              <TableRow key={invoice.id}>
+                <TableCell className="font-medium text-left p-0">
+                  <Link
+                    href={`/invoices/${invoice.id}`}
+                    className="font-semibold block p-4"
+                  >
+                    {new Date(invoice.timestamp).toLocaleDateString()}
+                  </Link>
+                </TableCell>
+                <TableCell className="text-left p-0">
+                  <Link
+                    href={`/invoices/${invoice.id}`}
+                    className="font-semibold block p-4"
+                  >
+                    Bob Belcher
+                  </Link>
+                </TableCell>
+                <TableCell className="text-left p-0">
+                  <Link href={`/invoices/${invoice.id}`} className="block p-4">
+                    bob@bobsburgers.com
+                  </Link>
+                </TableCell>
+                <TableCell className="text-center p-0">
+                  <Link href={`/invoices/${invoice.id}`} className="block p-4">
+                    <Badge className="rounded-full">{invoice.status}</Badge>
+                  </Link>
+                </TableCell>
+                <TableCell className="text-right p-0">
+                  <Link
+                    href={`/invoices/${invoice.id}`}
+                    className="font-semibold block p-4"
+                  >
+                    ${invoice.value}
+                  </Link>
+                </TableCell>
+              </TableRow>
+            );
+          })}
         </TableBody>
       </Table>
     </main>
